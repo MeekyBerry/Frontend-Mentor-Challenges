7ddd01d42cbe88b9beff94d541b0d548cfd65452
meeky berry <meekyberry6@gmail.com>
2022-08-19 01:27:44 +0100

    custom button function

--- tip-calculator-app/script.js
+++ tip-calculator-app/script.js
@@ -3,6 +3,9 @@ const TIP_PERCENT = document.querySelectorAll('.percent');
 const PEOPLE = document.querySelector('.number-input');
 const TOTAL_BILL = document.getElementById('total');
 const TIP_AMOUNT = document.getElementById('tip-amount');
+const CUSTOM = document.querySelector('.custom');
+const CUSTOM_BUTTON = document.getElementById('cst-btn')
+const CUSTOM_INPUT = document.getElementById('cst-input')
 
 let billValue;
 let person;
@@ -15,34 +18,45 @@ let error = true;
 let inputPeople = false;
 
 const calculateHandler = () => {
-	if (inputPeople) {
-      let amount = (billValue + tipAmount) / person;
-		TOTAL_BILL.textContent = `${amount.toFixed(2)}`;
-   }
+    if (inputPeople) {
+        let amount = (billValue + tipAmount) / person;
+        TOTAL_BILL.textContent = `${amount.toFixed(2)}`;
+    }
 };
 
 BILL.addEventListener('change', () => {
-	error = !error;
-	billValue = +BILL.value;
-	// console.log(billValue)
-	TIP_PERCENT.forEach((el, idx) => {
-		let percent = myArray[idx];
-		el.addEventListener('click', () => {
-			if (idx === 5) {
-				percent = prompt('enter tip percent');
-			}
-			tipAmount = BILL.value * (+percent / 100);
-			TIP_AMOUNT.textContent = `${tipAmount}`;
-			calculateHandler();
-		});
-	});
+    error = !error;
+    billValue = +BILL.value;
+    // console.log(billValue)
+    TIP_PERCENT.forEach((el, idx) => {
+        let percent = myArray[idx];
+        el.addEventListener('click', () => {
+            if (CUSTOM.firstElementChild === CUSTOM_INPUT && idx === 5) {
+                console.log('yes')
+                CUSTOM_INPUT.addEventListener('change', () => {
+                    console.log('yes')
+                    percent = 20;
+                })
+            }
+            tipAmount = BILL.value * (+percent / 100);
+            TIP_AMOUNT.textContent = `${tipAmount}`;
+            calculateHandler();
+        });
+    });
 });
 
 PEOPLE.addEventListener('input', () => {
-	inputPeople = !inputPeople;
-	person = +PEOPLE.value;
-	// console.log(person)
-	if (!error) {
-		calculateHandler();
-	}
+    inputPeople = !inputPeople;
+    person = +PEOPLE.value;
+    // console.log(person)
+    if (!error) {
+        calculateHandler();
+    }
 });
+
+CUSTOM.addEventListener('click', () => {
+    if (CUSTOM.firstElementChild === CUSTOM_BUTTON) {
+        CUSTOM.innerHTML = `<input type="number" id="cst-input" class="percent" placeholder="Custom" />`
+    }
+
+})
